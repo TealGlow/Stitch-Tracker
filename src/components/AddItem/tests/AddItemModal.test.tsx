970599b0c4cdf24caf8d0716fd6375c94dd56999
Luna <alyssacomstock95@gmail.com>
2026-02-10 09:22:15 -0800

    add tests

--- src/components/AddItem/tests/AddItemModal.test.tsx
+++ src/components/AddItem/tests/AddItemModal.test.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import AddItemModal, { modalType } from "../AddItemModal";
+import '@testing-library/jest-dom';
 
 describe("AddItemModal", () => {
     it("renders new project title", () => {
